3d508c59e1d05ed4a3a176aa843a2e8aeba6aad4
Eliza Moore <elzmoore13@gmail.com>
2018-06-11 19:05:22 +0000

    update function to include request

--- download_avatars.js
+++ download_avatars.js
@@ -3,6 +3,10 @@ var request = require('request');
 console.log("\nHey there!\n\nWelcome to the GitHub Avatar Downloader!\n")
 
 function getRepoContributors(repoOwner, repoName, cb){
+  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request(url, function(err, res, body){
+    cb(err, body);
+  });
 
 }
 
